8eb714703c8b841d085642962eb87ce5c0dd624b
Thijs-B-05 <thijsbuikema@gmail.com>
2026-07-08 18:33:52 +0200

    added working asteroids

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from logger import log_state
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT)
 from circleshape import CircleShape
 from player import Player
+from asteroids import Asteroids
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
@@ -10,10 +12,14 @@ def main():
 	dt = 0.0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n Screen width: 1280\n Screen height: 720")
+	asteroids = pygame.sprite.Group()
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	Player.containers = (updatable, drawable)
+	Asteroids.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
 	player = Player(x = SCREEN_WIDTH /2, y = SCREEN_HEIGHT / 2)
+	asteroidfield = AsteroidField()
 	while True:
 		log_state()
 		for event in pygame.event.get():
