3fbaaca0317d402fa8bb7ad907735eefb6df8a81
Thijs-B-05 <thijsbuikema@gmail.com>
2026-07-08 11:44:08 +0200

    Starting up for asteroids

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+from constants import (SCREEN_WIDTH, SCREEN_HEIGHT)
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n Screen width: 1280\n Screen height: 720")
+
+if __name__ == "__main__":
+    main()
