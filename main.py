81d6d6d57968b6fcf1064c03f6eff12e08105a5e
Thijs-B-05 <thijsbuikema@gmail.com>
2026-07-08 17:59:40 +0200

    Changed loop towards class groups and update and draw from within group

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 	dt = 0.0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n Screen width: 1280\n Screen height: 720")
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(x = SCREEN_WIDTH /2, y = SCREEN_HEIGHT / 2)
 	while True:
 		log_state()
@@ -17,8 +20,9 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
-		player.draw(screen)
-		player.update(dt)
+		for player in drawable:
+			player.draw(screen)
+		updatable.update(dt)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 		
